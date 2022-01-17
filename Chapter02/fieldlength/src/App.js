1822f7691294db4576730c5d97f0194d84945fad
Jerzy Supryn (GeSup) <skuter6@interia.pl>
2022-01-17 12:06:20 +0100

    my solution for Activity 2.01

--- Chapter02/fieldlength/src/App.js
+++ Chapter02/fieldlength/src/App.js
@@ -2,8 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fieldValue: "",
+      submitDisabled: true,
+    }
+  }
+
+  textareaValueChanged(event) {
+    const fieldValue = event.target.value;
+    this.setState({ fieldValue });
+    this.validateTextareaLength();
+  }
+
+  renderInputLength() {
+    return (<p>You entered {this.state.fieldValue.length} characters.</p>)
+  }
+
+  validateTextareaLength() {
+    if (!this.state.submitDisabled && this.state.fieldValue.length <= 100) {
+      this.setState({ submitDisabled: true });
+    } else if (this.state.submitDisabled && this.state.fieldValue.length > 100){
+      this.setState({ submitDisabled: false });
+    }
+  }
+
+  submitPost(){
+    alert("Submitting the blog post");
+  }
+
   render() {
-    return
+    return (
+      <div className="App"><h2>Blog Post Writer</h2>
+        <div>You must enter at least 100 characters to submit.</div>
+        <textarea onChange={this.textareaValueChanged.bind(this)}></textarea>
+        {this.renderInputLength()} <button disabled={this.state.submitDisabled} onClick={this.submitPost}>Submit Post</button>
+      </div>
+    )
   }
 }
 
