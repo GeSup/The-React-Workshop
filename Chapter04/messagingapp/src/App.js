fa62ab64d40855e5ff7974246a5bd390f0a4dabf
Jerzy Supryn (GeSup) <skuter6@interia.pl>
2022-01-18 11:08:15 +0100

    init messagingapp

---
+++ Chapter04/messagingapp/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (<div className="App"><h2>Messaging App</h2></div>)
+  }
+}
+
+export default App;
